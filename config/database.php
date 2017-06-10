8c8c74b14faaf667252a621861aa6daf0cba255b
Daniel Pena <daniel.pena@intec.edu.do>
2017-06-10 18:54:52 -0400

    pgsql configured with heroku

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-107-20-226-93.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd3voulsmv3odvk'),
+            'username' => env('DB_USERNAME', 'bgmfcywlmoxnbb'),
+            'password' => env('DB_PASSWORD', 'c958ba5154ee90b763d948c95ceb2c788d7a5eafa2f076dae300cd751ce220e3'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
