4ab7e9ee58481b52467de0930f161229a21f5770
Daniel <daniel.pena@intec.edu.do>
2017-06-12 11:37:10 -0400

    Acerca de enhaced

--- resources/views/others/about.blade.php
+++ resources/views/others/about.blade.php
@@ -5,15 +5,64 @@
 	<hr>
 	<div class="container myText">
 
-		<p><strong>Objetivos</strong></p>
 		<p>
-			-> Tener listados todos los profesores de todas las universidades de la República Dominicana.
+			<strong>Objetivos</strong>
 		</p>
 		<p>
-			-> Crear una plataforma donde el estudiante pueda acceder a información y críticas sobre cualquier profesor antes de tomar alguna asignatura con el mismo.
+			1. Formar una comunidad donde cada quién sea libre de expresar sus
+			opiniones acerca de un profesor.
 		</p>
-		<p><strong>Misión</strong></p>
-		<p><strong>Visión</strong></p>
+		<p>
+			2. Servir como guía al profesor para que vea qué opinan sobre él sus
+			estudiantes.
+		</p>
+		<p>
+			3. Proporcionar estadísticas sobre cada profesor. Los datos para las mismas
+			los proporcionarán los estudiantes al completar un formulario de valoración
+			en la página del profesor.
+		</p>
+		<p>
+			<strong>Misión</strong>
+		</p>
+		<p>
+			Ser la comunidad líder en los países de habla hispana que muestre
+			información sobre todos los profesores de todas las universidades de los
+			mismo. En suma, ser la fuente de información número 1 de búsqueda sobre las
+			opiniones de cualquier profesor en los países de habla hispana.
+		</p>
+		<p>
+			<strong>Valores</strong>
+		</p>
+		<ul>
+			<li>
+				Libertad
+			</li>
+			<li>
+				Participación
+			</li>
+			<li>
+				Compromiso
+			</li>
+			<li>
+				Confianza
+			</li>
+			<li>
+				Sinceridad
+			</li>
+			<li>
+				Iniciativa
+			</li>
+			<li>
+				Cooperación
+			</li>
+			<li>
+				Conciencia
+			</li>
+			<li>
+				Honestidad
+			</li>
+		</ul>
+
 
 	</div>
 @endsection
